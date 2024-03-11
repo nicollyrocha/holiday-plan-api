6cb88fcbf36a9608b8c8109dc6438b3e27974d0e
Nicolly Rocha <nicollyrcs@gmail.com>
2024-03-11 20:10:14 -0300

    commiting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import 'express-async-errors';
 import express from 'express';
+import { Router } from 'express';
 import cors from 'cors';
 const bodyparser = require('body-parser');
 import { router } from './server/routes/index';
+const holidayController = require('../../controllers/holidays.controller');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const routerExpress = Router();
 
 app.use(cors());
 
@@ -16,8 +19,12 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+/* router.get('/', holidayController.getHolidays);
+router.post('/holiday', holidayController.createHoliday);
+router.put('/holiday', holidayController.updateHoliday);
+router.delete('/holiday/:id', holidayController.deleteHoliday); */
 app.use(express.json());
-app.use(router);
+app.use('/', router);
 app.use(bodyparser.urlencoded({ extended: false }));
 
 module.exports = app;
