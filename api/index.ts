0906e45f06a15864006c7683e38910dd0a8e64bc
Nicolly Rocha <nicollyrcs@gmail.com>
2024-03-11 18:54:48 -0300

    commiting

--- api/index.ts
+++ api/index.ts
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const PORT = parseInt(`${process.env.PORT || 3000}`);
-
-const { app } = require('../app');
-
-app.listen(PORT, () => console.log(`Server is running at ${PORT}.`));
