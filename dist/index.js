5a3b09f68c9951eccca6aa0fae28be6215ddb14a
Nicolly Rocha <nicollyrcs@gmail.com>
2024-03-11 19:33:16 -0300

    commiting

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("express-async-errors");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./server/routes");
+const bodyparser = require('body-parser');
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 5000;
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => res.send('Express on Vercel'));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+app.use(express_1.default.json());
+app.use(routes_1.router);
+app.use(bodyparser.urlencoded({ extended: false }));
+module.exports = app;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
-import { router } from './src/server/routes';
+import { router } from './server/routes';
 const bodyparser = require('body-parser');
 
 const app = express();
@@ -9,12 +9,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
-
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+app.use(express.static('public'));
 app.use(express.json());
 app.use(router);
 app.use(bodyparser.urlencoded({ extended: false }));
